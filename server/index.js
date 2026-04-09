7acf72a99ce451b5964792e22fec81b69dc35d0b
Agniva-07 <haitagniva@gmail.com>
2026-04-09 14:38:55 +0530

    changed port

--- server/index.js
+++ server/index.js
@@ -95,7 +95,7 @@ async function start() {
     throw err;
   }
 
-  const PORT = process.env.PORT || 5000;
+  const PORT = process.env.PORT || 8080;
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
